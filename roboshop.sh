7d7d6595bee0f7645b7ea0705247da629032b4ee
mail2chandrasheker <mail2chandrasheker@gmail.com>
2025-09-28 09:29:14 +0530

    update the instance

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,7 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-00dac7c720709bc8e"
+
 for instance in $@
 do
    instance_id=$(aws ec2 run-instances \
@@ -14,14 +15,14 @@ do
 
   #get privete ip and public ip
   if [ $instance !="frontend " ]; then
-       ip=$(aws ec2 describe-instances \
-  --instance-ids $instance_id \
-  --query "Reservations[0].Instances[0].PublicIpAddress" \
-  --output text)
-  else
-       ip=$(aws ec2 describe-instances \
+        ip=$(aws ec2 describe-instances \
   --instance-ids $instance_id \
   --query "Reservations[0].Instances[0].PrivateIpAddress" \
+  --output text)
+  else
+      ip=$(aws ec2 describe-instances \
+  --instance-ids i-083dd8f16722b1755 \
+  --query "Reservations[0].Instances[0].PublicIpAddress" \
   --output text)
 
    fi

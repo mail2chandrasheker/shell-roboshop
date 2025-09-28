ea9de3f1150001c0eff5588c30bb474fb7e5846c
mail2chandrasheker <mail2chandrasheker@gmail.com>
2025-09-28 10:51:18 +0530

    updated

--- mongodb.sh
+++ mongodb.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
 
-
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-Y="\e[033m"
-#validate the user is root or normla
-userid=$(id  -u)
+Y="\e[33m"
+
+userid=$(id -u)
 
 logs_folder="/var/log/shell-roboshop"
-script_name=$( echo $0 | cut -d "." -f1 )
+script_name=$(basename $0 | cut -d "." -f1)
 log_file="$logs_folder/$script_name.log"
 
 mkdir -p $logs_folder
 
-echo "script started executed at: $(date)" | tee -a $log_file
-if [ "$userid"   -ne  0 ] ; then
-   echo -e "$R Error:: you are  not root user $N" | tee -a $log_file
+echo "Script started execution at: $(date)" | tee -a $log_file
+
+if [ "$userid" -ne 0 ] ; then
+   echo -e "$R Error: You are not root user. Please run as root. $N" | tee -a $log_file
    exit 1
 fi
 
-validate(){
-    if [ $1 -ne  0 ]; then
-        echo -e "$1....$R Failure $N" | tee -a $log_file
+validate() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 .... $R Failure $N" | tee -a $log_file
+        exit 1
     else
-        echo -e "$1.....$G success $N" | tee -a $log_file
+        echo -e "$2 .... $G Success $N" | tee -a $log_file
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-validate $? "Adding Mongo repo"
+# Add MongoDB repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo 2>&1 | tee -a $log_file
+validate $? "Adding MongoDB repo"
 
-dnf install mongodb-org -y &>>#$log_file
+# Install MongoDB
+dnf install -y mongodb-org 2>&1 | tee -a $log_file
 validate $? "Installing MongoDB"
 
-systemctl enable mongod  &>>#$log_file
-validate $? "Enable Mongodb"
+# Enable MongoDB
+systemctl enable mongod 2>&1 | tee -a $log_file
+validate $? "Enabling MongoDB service"
 
-systemctl start mongod  &>>#$log_file
-validate $? "Start Mongodb"
+# Start MongoDB
+systemctl start mongod 2>&1 | tee -a $log_file
+validate $? "Starting MongoDB service"

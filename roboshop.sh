73b70c2fe2941d5a0c88606a222c37dbf7d86bfe
mail2chandrasheker <mail2chandrasheker@gmail.com>
2025-09-28 10:16:08 +0530

    udpate istance and records

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-00dac7c720709bc8e"
+Hosted_Zone_ID="Z0033676TLKDOCFXBMTY"
+Domain_Name="chandradevops.space"
 
 for instance in "$@"
 do
@@ -16,18 +18,36 @@ do
    # wait until instance is running
    aws ec2 wait instance-running --instance-ids $instance_id
 
-   # get private ip (default) or public ip (frontend)
+   # get IP and set record name
    if [ "$instance" != "frontend" ]; then
         ip=$(aws ec2 describe-instances \
           --instance-ids $instance_id \
           --query "Reservations[0].Instances[0].PrivateIpAddress" \
           --output text)
+        RECORD_NAME="$instance.$Domain_Name"
    else
         ip=$(aws ec2 describe-instances \
           --instance-ids $instance_id \
           --query "Reservations[0].Instances[0].PublicIpAddress" \
           --output text)
+        RECORD_NAME="$Domain_Name"
    fi
 
-   echo "$instance: $ip"
+   echo "$instance: $ip ($RECORD_NAME)"
+
+   # Update Route53 record
+   aws route53 change-resource-record-sets \
+     --hosted-zone-id $Hosted_Zone_ID \
+     --change-batch "{
+       \"Comment\": \"Update $RECORD_NAME\",
+       \"Changes\": [{
+         \"Action\": \"UPSERT\",
+         \"ResourceRecordSet\": {
+           \"Name\": \"$RECORD_NAME\",
+           \"Type\": \"A\",
+           \"TTL\": 60,
+           \"ResourceRecords\": [{\"Value\": \"$ip\"}]
+         }
+       }]
+     }"
 done
